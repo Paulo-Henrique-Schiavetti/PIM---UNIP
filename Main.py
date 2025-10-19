3f9b67451556ee0b3a19656f00339bd61a8cceb8
Paulo <paulohenriqueschiavetti@hotmail.com>
2025-10-19 17:08:45 -0300

    Integração da pate gráfica e banco de dados

--- Main.py
+++ Main.py
@@ -24,54 +24,28 @@ nome_usuario = ""
 
 # FUNÇÕES DE LOGIN
 
-def login_aluno():
-    print(texto2)
-    continuar = 3
-    while continuar>0:
-        ra = input("Digite o seu RA: ")
-        senha = input("Digite a sua senha: ")
+def login_aluno(ra, senha):
         aluno = cursor.execute('SELECT * FROM alunos WHERE ra=? AND senha=?', (ra, senha)).fetchone()
         if aluno != None:
+            usuario_adm = False
             id_usuario = aluno[0]
             ra_usuario = aluno[1]
             nome_usuario = aluno[3]
-            break
-        print(f"Usuario ou senha incorretos, voce tem mais {continuar} chances. ")
-        continuar -= 1
-    print(f"\n\nSeja Bem-vindo {nome_usuario}!\n")
+            return True
+        else:
+            return False
 
-def login_adm():
-    print(texto3)
-    continuar = 3
-    while continuar>0:
-        login = input("Digite o seu login: ")
-        senha = input("Digite a sua senha: ")
-        aluno = cursor.execute('SELECT * FROM administrador WHERE login=? AND senha=?', (login, senha)).fetchone()
-        if aluno != None:
-            id_usuario = aluno[0]
-            ra_usuario = aluno[1]
-            nome_usuario = aluno[3]
-            break
-        print(f"Usuario ou senha incorretos, voce tem mais {continuar} chances. ")
-        continuar -= 1
-    print(f"\n\nSeja Bem-vindo {nome_usuario}!\n")
-
-# PARTE INICIAL DO PROGRAMA
-
-print(texto1)
-
-print("Digite <1> para entrar como aluno. \nDigite <2> para entrar como administrador. \nDigite <0> para encerrar o programa.")
-resposta = int(input("resposta: "))
-
-while resposta !=0:
-    if resposta == 1:
-        login_aluno()
-        break
-    elif resposta == 2:
-        login_adm()
-        break
-    else:
-        print("Número inválido.")
-        resposta = int(input("resposta: "))
-
-print("Fim do programa.")
+def login_adm(login, senha):
+        usuario = cursor.execute('SELECT * FROM administrador WHERE login=? AND senha=?', (login, senha)).fetchone()
+        if usuario != None:
+            usuario_adm = True
+            id_usuario = usuario[0]
+            ra_usuario = usuario[1]
+            nome_usuario = usuario[3]
+            return True
+        else:
+            return False
+        
+def encerrar():
+     con.commit()
+     con.close()
